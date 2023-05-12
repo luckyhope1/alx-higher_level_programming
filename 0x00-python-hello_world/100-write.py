94702e0a8c1607cae546cd404c9e7ca1859e7ae7
luckyhope1 <geoffryoduor92@gmail.com>
2023-05-12 05:33:19 -0700

    100-write.py

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-import sys
+from sys import stderr
 
-message = "that piece of art is useful - Dora Korpar, 2015-10-19\n"
-
-sys.stderr.write(message)
-sys.exit(1)
+stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+exit(1)
