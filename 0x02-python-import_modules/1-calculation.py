fa22e398f4c746a1e86b84f2a512114865fd87e6
luckyhope1 <geoffryoduor92@gmail.com>
2023-05-12 09:54:36 -0700

    1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,11 +3,7 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
-    sum_ab = add(a, b)
-    diff_ab = sub(a, b)
-    prod_ab = mul(a, b)
-    quot_ab = div(a, b)
-    print("The sum of", a, "and", b, "is", sum_ab)
-    print("The difference between", a, "and", b, "is", diff_ab)
-    print("The product of", a, "and", b, "is", prod_ab)
-    print("The quotient of", a, "and", b, "is", quot_ab)
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
