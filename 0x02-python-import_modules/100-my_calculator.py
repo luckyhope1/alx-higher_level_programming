d813d0546f30a6aa1d30f54245806e3dbaab7eb5
luckyhope1 <geoffryoduor92@gmail.com>
2023-05-12 10:27:43 -0700

    100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,27 +1,22 @@
 #!/usr/bin/env python3
 import sys
 from calculator_1 import add, sub, mul, div
-
-if __name__ == '__main__':
-    if len(sys.argv) != 4:
+if __name__ == "__main__":
+    num_args = len(sys.argv)
+    if num_args != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-
     a = int(sys.argv[1])
     op = sys.argv[2]
     b = int(sys.argv[3])
-
-    if op not in ['+', '-', '*', '/']:
+    if op == '+':
+        print("{} {} {} = {}".format(a, op, b, add(a, b)))
+    elif op == '-':
+        print("{} {} {} = {}".format(a, op, b, sub(a, b)))
+    elif op == '*':
+        print("{} {} {} = {}".format(a, op, b, mul(a, b)))
+    elif op == '/':
+        print("{} {} {} = {}".format(a, op, b, div(a, b)))
+    else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
-
-    if op == '+':
-        res = add(a, b)
-    elif op == '-':
-        res = sub(a, b)
-    elif op == '*':
-        res = mul(a, b)
-    elif op == '/':
-        res = div(a, b)
-
-    print("{:d} {:s} {:d} = {:d}".format(a, op, b, res))
