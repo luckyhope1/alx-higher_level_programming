e4538a19400c078d5021b93818b81f5ac6dfee44
luckyhope1 <geoffreyoduor92@gmail.com>
2023-06-13 18:44:31 -0700

    12-pascal_triangle.py

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,41 +1,21 @@
-!/usr/bin/python3
-"""
-
-returns int lists of pascal triangle of any given size
-"""
+#!/usr/bin/python3
+"""Defines a Pascal's Triangle function."""
 
 
 def pascal_triangle(n):
-    """
-    Return:
-        empty list [] if n <= 0
-        if n is 7, we should expect:
-            [1]
-            [1, 1]
-            [1, 2, 1]
-            [1, 3, 3, 1]
-            [1, 4, 6, 4, 1]
-            [1, 5, 10, 10, 5, 1]
-            [1, 6, 15, 20, 15, 6, 1]
+    """Represent Pascal's Triangle of size n.
+
+    Returns a list of lists of integers representing the triangle.
     """
     if n <= 0:
         return []
-    if n == 1:
-        return [[1]]
 
-    triangle = [[1]]
-    for rows in range(n-1):
-        triangle.append([a+b for a, b
-                         in zip([0] + triangle[-1], triangle[-1] + [0])])
-    return triangle
-
-# C implementation of the last Pythonic loop
-#
-#    triangle = [[1]]
-#    for rows in range(n-1):
-#        l = [1]
-#        for i in range(rows):
-#            l.append(triangle[-1][i] + triangle[-1][i+1])
-#        l.append(1)
-#        triangle.append(l)
-#    return triangle
+    triangles = [[1]]
+    while len(triangles) != n:
+        tri = triangles[-1]
+        tmp = [1]
+        for i in range(len(tri) - 1):
+            tmp.append(tri[i] + tri[i + 1])
+        tmp.append(1)
+        triangles.append(tmp)
+    return triangles
