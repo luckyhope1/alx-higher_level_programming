3436f3d1152b54cde9dbd474fbbdb0f3a8da3526
luckyhope1 <geoffryoduor@gmail.com>
2023-07-18 11:07:58 -0700

    6-list_values.sql

--- 0x0D-SQL_introduction/6-list_values.sql
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -1,2 +1,3 @@
--- Script to list all rows of the first_table from the hbtn_0c_0 database in MySQL server
-SELECT * FROM 'first_table';
+-- Script to list all rows of the first_table from the hbtn_0c_0 database in
+-- MySQL server
+SELECT * FROM first_table;
