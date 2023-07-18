8abf4c7023c9347c83523c0c7e29bc74b5cd9acc
luckyhope1 <geoffryoduor@gmail.com>
2023-07-18 11:10:18 -0700

    5-full_table.sql

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,2 +1,3 @@
--- Script to print the full description of the first_table from the hbtn_0c_0 database in MySQL server
+-- Script to print the full description of the first_table from the hbtn_0c_0
+-- database in MySQL server
 SHOW CREATE TABLE 'first_table';
