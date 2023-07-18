6de0e7c777e3885a8b4bb27d747609c8a2bc4c2b
luckyhope1 <geoffryoduor@gmail.com>
2023-07-18 10:04:34 -0700

    0-list_databases.sql

--- 0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -1,4 +1,2 @@
-# Script to list all databases on MySQL server
-
-# Connect to the MySQL server and execute the SHOW DATABASES query
+-- Script to list all databases on MySQL server
 SHOW DATABASES;
