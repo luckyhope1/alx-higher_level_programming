78f88b934e8aa489dcadcd733e46b3e4d6f8fe7c
luckyhope1 <geoffryoduor@gmail.com>
2023-07-19 13:09:11 -0700

    101-avg_temperatures.sql

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,6 +1,6 @@
 -- Script to import table dump and display average temperature (in Fahrenheit)
 -- by city, ordered by temperature (descending)
-SELECT city, AVG(temperature) AS average_temperature_fahrenheit
-FROM tablename
-GROUP BY city
-ORDER BY average_temperature_fahrenheit DESC;
+SELECT `city`, AVG(`value`) AS `avg_temp`
+FROM `temperatures`
+GROUP BY `city`
+ORDER BY `avg_temp` DESC;
