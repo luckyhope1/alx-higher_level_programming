75b66a2f49b9d8d84b6b3b1675a0e52fe04d84d6
luckyhope1 <geoffryoduor@gmail.com>
2023-07-19 12:19:48 -0700

    9-cities_by_state_join.sql

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,6 @@
+-- Task: List all cities with their state names from the hbtn_0d_usa database
+SELECT c.`id`, c.`name`, s.`name`
+  FROM `cities` AS c
+       INNER JOIN `states` AS s
+       ON c.`state_id` = s.`id`
+ ORDER BY c.`id`;
