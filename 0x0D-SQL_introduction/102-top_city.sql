ec7cbc00091fee9d3e296b05b168bbcee84ba03e
luckyhope1 <geoffryoduor@gmail.com>
2023-07-19 13:10:40 -0700

    102-top_city.sql

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,7 +1,8 @@
 -- Script to import table dump and display the top 3 cities with the highest
 -- temperature during July and August, ordered by temperature (descending)
-SELECT city, temperature
-           FROM tablename
-           WHERE month IN (7, 8)
-           ORDER BY temperature DESC
-           LIMIT 3;
+SELECT `city`, AVG(`value`) AS `avg_temp`
+FROM `temperatures`
+WHERE `month` = 7 OR `month` = 8
+GROUP BY `city`
+ORDER BY `avg_temp` DESC
+LIMIT 3;
