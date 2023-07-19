48e769bd31e0e3daa22f5c6acdbed31368a5eb3c
luckyhope1 <geoffryoduor@gmail.com>
2023-07-19 12:19:14 -0700

    7-cities.sql

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,8 @@
+-- Task: List all cities of California from the hbtn_0d_usa database
+SELECT `id`, `name`
+  FROM `cities`
+ WHERE `state_id` IN
+       (SELECT `id`
+	        FROM `states`
+	       WHERE `name` = "California")
+ORDER BY `id`;
