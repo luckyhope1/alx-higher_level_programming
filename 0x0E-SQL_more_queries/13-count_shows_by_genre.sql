3093599e81714d5dcdc409b331a85c9da58817da
luckyhope1 <geoffryoduor@gmail.com>
2023-07-19 12:34:42 -0700

    13-count_shows_by_genre.sql

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,11 @@
+-- Task: List all genres with the number of shows linked to each from the
+-- hbtn_0d_tvshows database
+SELECT `name`, SUM(`rate`) AS `rating`
+  FROM `tv_genres` AS g
+       INNER JOIN `tv_show_genres` AS s
+       ON s.`genre_id` = g.`id`
+
+       INNER JOIN `tv_show_ratings` AS r
+       ON r.`show_id` = s.`show_id`
+ GROUP BY `name`
+ ORDER BY `rating` DESC;
