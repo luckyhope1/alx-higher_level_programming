61bd89b63c9955fa977ad476d0e09fd33a038e82
luckyhope1 <geoffryoduor@gmail.com>
2023-07-19 12:34:11 -0700

    11-genre_id_all_shows.sql

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,6 @@
+-- Task: List all shows with their genre IDs from the hbtn_0d_tvshows database
+SELECT s.`title`, g.`genre_id`
+  FROM `tv_shows` AS s
+       LEFT JOIN `tv_show_genres` AS g
+       ON s.`id` = g.`show_id`
+ ORDER BY s.`title`, g.`genre_id`;
