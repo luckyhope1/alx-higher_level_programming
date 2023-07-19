e8ace5fca16332cd616c69ccade3a3662adf7871
luckyhope1 <geoffryoduor@gmail.com>
2023-07-19 12:08:43 -0700

    6-states.sql

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,6 @@
+-- Task: Create database hbtn_0d_usa and table states in it
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+       id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
+       name VARCHAR(256) NOT NULL
+);
