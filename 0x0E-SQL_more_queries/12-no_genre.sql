599a54183f1cd9673084efdf1edb6621b8477a0e
luckyhope1 <geoffryoduor@gmail.com>
2023-07-19 12:34:25 -0700

    12-no_genre.sql

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,8 @@
+-- Task: List all shows without a genre linked from the hbtn_0d_tvshows
+-- database
+SELECT s.`title`, g.`genre_id`
+  FROM `tv_shows` AS s
+       LEFT JOIN `tv_show_genres` AS g
+       ON s.`id` = g.`show_id`
+       WHERE g.`genre_id` IS NULL
+ ORDER BY s.`title`, g.`genre_id`;
