5c36d1ec79387a17ad2cfe039e743d28d050a5ad
luckyhope1 <geoffryoduor@gmail.com>
2023-08-16 16:48:36 -0700

    10-factorial.js

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+function factorial (n) {
+  if (isNaN(n) || n === 0) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
+
+const arg = process.argv[2];
+const num = parseInt(arg);
+
+const result = factorial(num);
+console.log(result);
