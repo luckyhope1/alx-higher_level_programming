864590da06293843b254285740555a21e53c7568
Geoffrey Onyango Oduor <124439431+luckyhope1@users.noreply.github.com>
2023-08-31 23:04:11 +0300

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/node
-
-exports.addMeMaybe = (num, func) => {
-  func(num + 1);
-};
--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-
-const myVar = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-
-for (const line of myVar) {
-  console.log(line);
-}
